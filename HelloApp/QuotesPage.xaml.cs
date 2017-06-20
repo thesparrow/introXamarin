2c6d28a4d49823cceae9490735ad80ac9be38453
Anna <Anya@Annas-MacBook-Air.local>
2017-06-20 10:20:12 -0700

    adding basic XAML design projects

---
+++ HelloApp/QuotesPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace HelloApp
+{
+    public partial class QuotesPage : ContentView
+    {
+        public QuotesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
