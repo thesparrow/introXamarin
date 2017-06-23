e9505abc04aa7acccaed614b7753b19abdd0cef9
Anna <Anya@Annas-MacBook-Air.local>
2017-06-23 16:37:37 -0700

    Adding item selections and tapping events on the list

---
+++ HelloApp/SelectionList.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloApp
+{
+    public partial class SelectionList: ContentPage
+    {
+        public SelectionList()
+        {
+            InitializeComponent();
+        }
+    }
+}
