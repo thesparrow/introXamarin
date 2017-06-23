c017c66e6675db5860d5cd2d973772ffcd70e604
Anna <Anya@Annas-MacBook-Air.local>
2017-06-23 14:50:41 -0700

    Adding lists and group displays

---
+++ HelloApp/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HelloApp.Models
+{
+    public class Contact
+    {
+        public Contact()
+        {
+        }
+    }
+}
---
+++ HelloApp/Models/ContactGroup.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HelloApp.Models
+{
+    public class ContactGroup
+    {
+        public ContactGroup()
+        {
+        }
+    }
+}
